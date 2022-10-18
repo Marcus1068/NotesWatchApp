871120959d66cf87548158c03e091e6c38270f42
Marcus Deuß <marcus.deuss@me.com>
2022-10-18 11:42:10 +0200

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Marcus Deuß on 18.10.22.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
