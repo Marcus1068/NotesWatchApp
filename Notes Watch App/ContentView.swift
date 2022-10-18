c83403f6b03a1bc6e1e0443ba60341ed8e76bf1f
Marcus Deuß <marcus.deuss@me.com>
2022-10-18 13:33:35 +0200

    initial

--- Notes Watch App/ContentView.swift
+++ Notes Watch App/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var notes = [Note]()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Notes: \(notes.count)")
+            
+            Button("Add Note"){
+                let note = Note(id: UUID(), text: "Example")
+                notes.append(note)
+            }
         }
-        .padding()
     }
 }
 
---
+++ Notes Watch App/Note.swift
@@ -0,0 +1,14 @@
+//
+//  Note.swift
+//  Notes Watch App
+//
+//  Created by Marcus Deuß on 18.10.22.
+//
+
+import Foundation
+
+
+struct Note: Identifiable {
+    let id: UUID
+    let text: String
+}
